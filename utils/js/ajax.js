2d51c833dcdf7249335f774d191e53cc6d8a82d7
mySkey <476168710@qq.com>
2019-04-07 14:05:59 +0800

    更换接口

--- utils/js/ajax.js
+++ utils/js/ajax.js
@@ -1,4 +1,4 @@
-let api_url = 'https://www.22family.com/music/ph/'
+let api_url = 'https://api.22family.com/music/ph/'
 export default{
   get(url, data){
     return new Promise((resolve, reject)=>{
